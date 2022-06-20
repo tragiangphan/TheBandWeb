735038c7341d181ecd3399d01e2d0889ad2e6c6b
River-Tea <tragiangphanthi2205@gmail>
2022-06-20 11:11:46 +0700

    Responsive menu bar

--- Sources/script/script.js
+++ Sources/script/script.js
@@ -1,13 +1,14 @@
-$(document).ready(function () {
-    const buyBtns = document.querySelectorAll(".js-btn-buy");
-    const modal = document.querySelector(".js-modals");
-    const close = document.querySelector(".js-btn-close");
+//===== Modal =====
+$(function () {
+    const buyBtns = document.querySelectorAll(".js-btn-buy")
+    const modal = document.querySelector(".js-modals")
+    const close = document.querySelector(".js-btn-close")
     const modalContainer = document.querySelector(".js-mod-container")
 
     function showBuyTickets() {
-        modal.classList.add('open')
+        modal.classList.add("open")
     }
-    
+
     function hideBuyTickets() {
         modal.classList.remove("open")
     }
@@ -19,8 +20,36 @@ $(document).ready(function () {
     close.addEventListener("click", hideBuyTickets)
 
     modal.addEventListener("click", hideBuyTickets)
-    
+
     modalContainer.addEventListener("click", function (event) {
-        event.stopPropagation();
-    })
+        event.stopPropagation()
+    });
+});
+
+//===== Menu bar =====
+$(function () {
+    var header = document.querySelector(".wrap__header")
+    var mobileMenu = document.querySelector(".head__menu-btn")
+    var headerHeight = header.clientHeight
+
+    //===== Open/close menu bar =====
+    mobileMenu.onclick = function () {
+        var isClosed = header.clientHeight === headerHeight
+        if(isClosed) {
+            header.style.height = 'auto'
+        }
+        else {
+            header.style.height = null
+        }
+    };
+
+    //===== Auto close menu bar =====
+    var menuItems = document.querySelectorAll('.head__nav li a[href *= "#"]')
+    for(var i = 0; i < menuItems.length; i++) {
+        var menuItem = menuItems[i]
+        menuItem.onclick = function () {
+            header.style.height = null;
+        }
+    }
+
 });
